9dfff656464e9b707b501be74c048a2c0a85f266
kathik-allu <134575855+kathik-allu@users.noreply.github.com>
2024-08-31 13:18:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,14 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
         except Exception as e:
             st.error(f"Failed to submit order: {e}")
-
+if ingredients_list:
+    ingredients_string = ""
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ", "
+    st.subheader(fruit_chosen + " Nutrition Information")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
